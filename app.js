52d638dcbc0302986652b3fe7a98df1992c9e940
bon4dev <bon4dev@gmail.com>
2025-02-18 17:36:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -204,10 +204,7 @@ app.delete('/tce/:idUnico', async (req, res) => {
   const { senha } = req.body;
   const folderPath = `./tce/${idUnico}`;
 
-  console.log(senha);
-  console.log(senha !== '123');
-
-  if (senha !== '123') {
+  if (senha !== '12345678') {
     return res.status(403).send({ message: 'Senha inválida.' });
   }
 
